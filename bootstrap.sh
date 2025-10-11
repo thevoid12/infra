0b03b863d16c8c820abfe90c3c37e8518165a921
thevoid12 <iamvinod2001@gmail.com>
2025-10-11 20:08:50 +0530

    configs updated

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,7 +16,7 @@ docker  compose up -d
 
 # Reload nginx to apply new configs
 echo "Reloading nginx..."
-docker exec nginx nginx -s reload
+sleep 5 && docker exec nginx nginx -s reload
 
 echo "Infra bootstrapped successfully. Woodpecker server and agent are running."
 echo "Access Woodpecker at http://localhost:8000 (or configured host)"
